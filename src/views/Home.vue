565ebf31ecf6896791d05d9d772853dbb5e22264
tomohikoseven <tomohikoseven0905@gmail.com>
2021-07-17 20:31:36 +0900

    vuejs3 データプロパティとメソッド

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
+  <div id="example1" class="demo">
+    <p>Using mustaches: {{ rawHtml }}</p>
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  </div>
+  <div v-bind:id="dynamicId">dynamicId</div>
+  <button :disabled="isButtonDisabled">Button</button>
+  <div :id="'list-' + id">list-1</div>
+  <a v-bind:[attributeName]="url">attributeName</a>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      rawHtml: '<span style="color: red">This should be red.</span>',
+      dynamicId: "ididid",
+      isButtonDisabled: true,
+      id: 1,
+      attributeName: "href",
+      url: "https://www.google.co.jp/",
+    };
   },
 });
 </script>
+
+<style lang="scss">
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
